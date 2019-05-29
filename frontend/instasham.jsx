2baa67086623646bbdda669d7342dc170c4b02e5
Tim Sakow <tcsakow@gmail.com>
2019-05-29 11:34:16 -0400

    Added reducers and store

--- frontend/instasham.jsx
+++ frontend/instasham.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, logout, createUser} from './util/session_api_util';
+import configureStore from './store/store';
 import * as seshApiUtil from './util/session_api_util';
 
 //testing
 window.login = login;
 window.logout = logout;
 window.createUser = createUser;
+const store = configureStore();
+window.getState = store.getState;
+window.dispatch = store.dispatch;
 
 
 document.addEventListener('DOMContentLoaded', () => {
