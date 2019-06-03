6d7f0f2277d1cd78afba6b959ad08993c2ef3f5b
Tim Sakow <tcsakow@gmail.com>
2019-06-03 08:08:17 -0400

    Added PostApiUtil to util

---
+++ frontend/util/post_api_util.js
@@ -0,0 +1,38 @@
+export const fetchPosts = () => (
+    $.ajax({
+        method: 'GET',
+        url: `api/posts`,
+        data: { post }
+    })
+);
+
+export const fetchPost = (post) => (
+    $.ajax({
+        method: 'GET',
+        url: `api/posts/${post.id}`,
+        data: { post }
+    })
+);
+
+export const createPost = (post) => (
+    $.ajax({
+        method: 'POST',
+        url: 'api/posts',
+        data: { post }
+    })
+);
+
+export const updatePost = (post) => (
+    $.ajax({
+        method: 'PATCH',
+        url: `api/posts/${post.id}`,
+        data: {post}
+    })
+);
+
+export const deletePost = (postId) => (
+    $.ajax({
+        method: 'DELETE',
+        url: `api/posts/${postId}`,
+    })
+);
