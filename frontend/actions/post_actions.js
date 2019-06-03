3b89a34883cecf6052e7c57d14c7cf3f5d059325
Tim Sakow <tcsakow@gmail.com>
2019-06-03 08:33:58 -0400

    Added PostActions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,41 @@
+import * as postApiUtil from '../util/post_api_util';
+
+export const RECEIVE_ALL_POSTS = "RECEIVE_ALL_POSTS";
+export const RECEIVE_POST = "RECEIVE_POST";
+export const REMOVE_POST = "REMOVE_POST";
+
+export const fetchPosts = () => dispatch => (
+    PostApiUtil.fetchPosts().then(posts => dispatch(receiveAllPosts(posts)))
+);
+
+export const fetchPost = id => dispatch => (
+    PostApiUtil.fetchPost(id).then(post => dispatch(receivePost(post)))
+);
+
+export const createPost = post => dispatch => (
+    PostApiUtil.createPost(post).then(post => dispatch(receivePost(post)))
+);
+
+export const updatePost = post => dispatch => (
+    PostApiUtil.updatePost(post)
+        .then(post => dispatch(receivePost(post)))
+);
+
+export const deletePost = postId => dispatch => (
+    PostApiUtil.deletePost(postId).then(post => dispatch(removePost(postId)))
+);
+
+export const receiveAllPosts = () => ({
+    type: RECEIVE_ALL_POSTS,
+    posts
+});
+
+export const receivePost = (post) => ({
+    type: RECEIVE_POST,
+    post
+});
+
+export const deletePost = (postId) => ({
+    type: REMOVE_POST,
+    PostId
+});
