c4834f292424f2d7aebe2fd154b3beb42361d8f5
Tim Sakow <tcsakow@gmail.com>
2019-06-25 14:22:23 -0400

    Change values in create post container

--- frontend/components/posts/create_post_form_container.jsx
+++ frontend/components/posts/create_post_form_container.jsx
@@ -5,12 +5,6 @@ import { fetchPosts, fetchPost, createPost } from '../../actions/post_actions';
 import CreatePostForm from './create_post_form';
 
 const mapStateToProps = (state, ownProps) => {
-<<<<<<< HEAD
-    const errs = state.errors.session;
-    const formType = 'Create Post';
-
-    return { errs, formType };
-=======
     return {
         post: {
             location: "",
@@ -19,17 +13,12 @@ const mapStateToProps = (state, ownProps) => {
             body: ''
         }
     }
->>>>>>> profiles
 };
 
 const mapDispatchToProps = dispatch => ({
     createPost: (post) => dispatch(createPost(post)),
-<<<<<<< HEAD
-    clearErrors: () => dispatch(clearErrors())
-=======
     fetchPosts: () => dispatch(fetchPosts()),
     fetchPost: (id) => dispatch(fetchPost(id))
->>>>>>> profiles
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreatePostForm);
