4cb0bea90a3b67ae10318e977d391a7cdb2dee89
Tim Sakow <tcsakow@gmail.com>
2019-07-08 14:48:28 -0400

    Add likesReducer, modify postsReducers for likes

--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -1,15 +1,24 @@
 import { RECEIVE_ALL_POSTS, RECEIVE_POST, REMOVE_POST } from '../actions/post_actions';
+import { RECEIVE_LIKE, REMOVE_LIKE } from '../actions/like_actions';
 import merge from 'lodash/merge';
 
 const postsReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
+    let newState = merge({}, oldState);
+
     switch( action.type ) {
+        case RECEIVE_LIKE:
+            newState[action.like.post_id].like_counter += 1;
+            newState[action.like.post_id].like_by_user = true;
+            return newState;
+        case REMOVE_LIKE:
+            newState[action.like.post_id].like_counter -= 1;
+            newState[action.like.post_id].like_by_user = false;
         case RECEIVE_ALL_POSTS:
             return action.posts;
         case RECEIVE_POST:
             return merge( {}, oldState, {[action.post.id]: action.post});
         case REMOVE_POST:
-            let newState = merge({}, oldState);
             delete newState[action.postId];
             return newState;
         default:
