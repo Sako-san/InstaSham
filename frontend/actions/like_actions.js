1cb4a700c16b4b5a4ee809748219463b1e77c5bc
Tim Sakow <tcsakow@gmail.com>
2019-07-08 14:11:30 -0400

    Add likes actions file

---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,29 @@
+import * as APIUtil from '../util/like_api_util';
+
+export const RECEIVE_LIKE = 'RECEIVE_LIKE';
+export const REMOVE_LIKE = 'REMOVE_LIKE';
+export const RECEIVE_LIKE_ERRORS = 'RECEIVE_LIKE_ERRORS';
+
+const receiveLike = (like) => ({
+    type: RECEIVE_LIKE,
+    like: like
+});
+
+const removeLike = (id) => ({
+    type: REMOVE_LIKE,
+    id: id,
+    post_id: id
+});
+
+const receiveErr = (errors) => ({
+    type: RECEIVE_LIKE_ERRORS,
+    errors: errors
+});
+
+export const createLike = (like) => (dispatch) => (
+    APIUtil.createLike(like).then((like) => dispatch(receiveLike(like)), errors => (dispatch(receiveErr(errors.responseJSON))))
+);
+
+export const deleteLike = (post_id) => (dispatch) => (
+    APIUtil.deleteLike(post_id).then((res) => dispatch(removeLike(post_id), errors => (dispatch(receiveErr(errors.responseJSON)))))
+);
