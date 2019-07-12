9bc43c01983cf283852456ddc7aa707208cc5064
Tim Sakow <tcsakow@gmail.com>
2019-07-12 09:55:01 -0400

    Add comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id           :bigint           not null, primary key
+#  comment_body :text             not null
+#  user_id      :integer          not null
+#  post_id      :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Comment < ApplicationRecord
+    validates :user_id, :post_id, presence: true
+    
+    belongs_to :user
+    belongs_to :post
+    
 end
