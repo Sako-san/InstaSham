9ca4344dc6c80ca7552a1eb17b2fad134ff2c774
Tim Sakow <tcsakow@gmail.com>
2019-07-19 14:13:40 -0400

    Add postShow URL

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,11 +6,13 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SplashContainer from './splash/splash_container';
 import PostIndexContainer from './posts/post_index_container';
 import UserProfileContainer from './users/user_profile_container';
+import PostShow from './posts/post_show';
 
 const App = () => (
     <section>
         <Switch>
             <ProtectedRoute exact path="/feed" component={PostIndexContainer} />
+            <ProtectedRoute exact path="/postShow" component={PostShow} />
             <ProtectedRoute exact path="/profile" component={UserProfileContainer} />
             <AuthRoute path="/signup" component={SignupContainer} />
             <AuthRoute path="/login" component={LoginContainer} />
---
+++ frontend/components/posts/post_show.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPost,  deletePost } from '../../actions/post_actions';
+import { createLike, deleteLike } from '../../actions/like_actions';
+import { createComment, fetchComments, deleteComment } from '../../actions/comment_actions';
+
+class PostShow extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <div>Post Show Page</div>
+        )
+    };
+};
+
+
+const mapStateToProps = (state) => {
+    state: state
+};
+
+const mapDispatchToProps = dispatch => ({
+    fetchPost: (id) => dispatch(fetchPost(id)),
+    deletePost: (id) => dispatch(deletePost(id)),
+
+    createLike: (like) => dispatch(createLike(like)),
+    deleteLike: (likeId) => dispatch(deleteLike(likeId)),
+
+    createComment: (comment) => dispatch(createComment(comment)),
+    deleteComment: (commentId) => dispatch(deleteComment(commentId)),
+    fetchComments: () => dispatch(fetchComments())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostShow)
